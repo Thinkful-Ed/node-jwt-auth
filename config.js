e0a6bcef435313095f876101860cf8d0b2dda66a
Joe Turner <joe@oampo.co.uk>
2017-06-01 17:22:58 +0100

    Rename database.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/basic-auth-demo';
+                      'mongodb://localhost/jwt-auth-demo';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
