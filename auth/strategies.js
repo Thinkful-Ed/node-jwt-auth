1d7642a3acf06ddcfeb2699bddbd4bdca32bafe0
Joe Turner <joe@oampo.co.uk>
2017-06-01 11:13:23 +0100

    Add comment on rejecting promises.

--- auth/strategies.js
+++ auth/strategies.js
@@ -17,6 +17,8 @@ const basicStrategy = new BasicStrategy((username, password, callback) => {
     .then(_user => {
       user = _user;
       if (!user) {
+        // Return a rejected promise so we break out of the chain of .thens.
+        // Any errors like this will be handled in the catch block.
         return Promise.reject({
           reason: 'LoginError',
           message: 'Incorrect username or password',
