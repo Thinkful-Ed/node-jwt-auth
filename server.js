d7475f19926371bc88c62c6d9f3de7982462a853
Chris Klanac <cklanac@users.noreply.github.com>
2017-10-30 16:21:29 -0400

    fix: mongoose deprecation warning

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ let server;
 
 function runServer() {
     return new Promise((resolve, reject) => {
-        mongoose.connect(DATABASE_URL, err => {
+        mongoose.connect(DATABASE_URL, {useMongoClient: true}, err => {
             if (err) {
                 return reject(err);
             }
