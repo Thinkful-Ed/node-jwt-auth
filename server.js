1405f99f59675560f1ee90cbe255a57abf817366
Chris Klanac <cklanac@users.noreply.github.com>
2018-02-27 23:13:07 -0600

    update mongoose

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ let server;
 function runServer(databaseUrl, port = PORT) {
 
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, { useMongoClient: true }, err => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
         return reject(err);
       }
